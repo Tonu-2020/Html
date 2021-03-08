e54794259726d3a69f3fbd9d70554340b8989528
Tonu-2020 <74651951+Tonu-2020@users.noreply.github.com>
2021-03-08 14:08:33 +0530

    Add files via upload

--- cycle 5/sample.php
+++ cycle 5/sample.php
@@ -1,26 +1,102 @@
 <html>
 <head></head>
 <body>
+<div align="center">
+<h1 style="text-align:center"><u>Insert Data</u></h1>
 <form method="post" action="sample.php" >
 <input type="text" name="name">
-<input type="submit" value="submit">
+branch<input type="text" name="branch"><br>
+mark<input type="text" name="mark"><br>
 
+
+<input type="submit" value="submit" name=submit>
+</form>
+</div>
+<div align="center">
+<form method="post" action="sample.php">
+<h1 style="text-align:center"><u>UPDATE DATA</u></h1>
+<input type="text" name="search_id" >
+<input type="submit" name="search" value="Search">
+<input type="submit" name="view" value="view">
+</form>
+</div>
 <?php
 $con=mysqli_connect("localhost","20mca054","2787","20mca054");
+if(isset($_POST["submit"]))
+{
 if($con)
 {
-
 echo $name=$_POST["name"];
-
-$qry="INSERT INTO student(name) VALUES ('$name')";
+echo $branch=$_POST["branch"];
+echo $mark=$_POST["mark"];
+$qry="INSERT INTO student(name,branch,mark) VALUES ('$name','$branch','$mark')";
 
 if(mysqli_query($con, $qry))
 {
-echo "";
+echo "Data inserted successfully<br>";
+}
+}
+}
+if(isset($_POST["search"]))
+{
+$id=$_POST["search_id"];
+$qry="select*from student where id='$id'";
+$data=mysqli_query($con, $qry);
+?>
+<h1 style="text-align:center"><u>STUDENT DETAILS</u></h1>
+<table align="center" border="1">
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>branch</th>
+<th>mark</th>
+
+</tr>
+<?php
+$res=mysqli_fetch_array($data);
+?>
+<tr>
+<td><?php echo $res['id'];?></td>
+<td><?php echo $res['name'];?></td>
+<td><?php echo $res['branch'];?></td>
+<td><?php echo $res['mark'];?></td>
+<td><a href="sample_update.php?id=<?php echo $res['id'];?>">update</a></td>
+<td><a href="sample_delete.php?id=<?php echo $res['id'];?>">Delete</a></td>
+</tr>
+</table>
+<?php
+}
+if(isset($_POST["view"]))
+{
+$qry="select*from student";
+$data=mysqli_query($con, $qry);
+?>
+<h1 style="text-align:center"><u>STUDENT DETAILS</u></h1>
+<table align="center" border="1">
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>branch</th>
+<th>mark</th>
+
+
+</tr>
+<?php
+while($res=mysqli_fetch_array($data))
+{
+?>
+<tr>
+<td><?php echo $res['id'];?></td>
+<td><?php echo $res['name'];?></td>
+<td><?php echo $res['branch'];?></td>
+<td><?php echo $res['mark'];?></td>
+
+
+</tr>
+<?php
 }
 }
 ?>
+</table>
 </body>
-</html>
-
 </html>
